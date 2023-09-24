d8130b4050442392f6cf84b8f8a823bda761a6ac
Daniel Sarmiento <josedanielsarmiento219@gmail.com>
2023-09-24 17:56:04 -0500

    add dockerfile to cars api

--- cars/main.js
+++ cars/main.js
@@ -1,6 +1,9 @@
 Bun.serve({
-    fetch(req) {
-      return new Response("Bun!");
-    },
-  });
-  
+  fetch(req) {
+    const url = new URL(req.url);
+    if (url.pathname === "/") return new Response("Home page! cars");
+    if (url.pathname === "/blog") return new Response("Cars Blog! ");
+    return new Response("404!");
+  },
+  port:8001,
+},);
