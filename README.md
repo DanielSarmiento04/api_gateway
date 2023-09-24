3452a584dc807d7b3a14ec783fb8db8d6dcab7f9
Daniel Sarmiento <josedanielsarmiento219@gmail.com>
2023-09-24 17:42:26 -0500

    add cars and diagrams

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ```
 sudo openssl req -new -newkey rsa:2048 -nodes -keyout tudominio.key -out tudominio.csr
 ```
+
+
+
+![](docs/Diagrams.png)
--- rust_api/src/main.rs
+++ rust_api/src/main.rs
@@ -1,4 +1,6 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, HttpRequest};
+use reqwest;
+
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -12,6 +14,34 @@ async fn echo(req_body: String) -> impl Responder {
 
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
+}
+
+const USERS_MICROSERVER_URL: &str = "http://127.0.0.1:8000"; // Replace with actual URL
+const CARS_MICROSERVER_URL: &str = "http://127.0.0.1:8082";  // Replace with actual URL
+
+
+// Users endpoint handler
+async fn users_handler(req: HttpRequest) -> HttpResponse {
+    let request_url = format!("{}{}", USERS_MICROSERVER_URL, req.uri());
+    match reqwest::Client::new().get(&request_url).send().await {
+        Ok(response) => {
+            HttpResponse::build(response.status())
+                .body(response.text().await.unwrap_or_default())
+        }
+        Err(_) => HttpResponse::InternalServerError().body("Error forwarding request to users micro server"),
+    }
+}
+
+// Cars endpoint handler
+async fn cars_handler(req: HttpRequest) -> HttpResponse {
+    let request_url = format!("{}{}", CARS_MICROSERVER_URL, req.uri());
+    match reqwest::Client::new().get(&request_url).send().await {
+        Ok(response) => {
+            HttpResponse::build(response.status())
+                .body(response.text().await.unwrap_or_default())
+        }
+        Err(_) => HttpResponse::InternalServerError().body("Error forwarding request to cars micro server"),
+    }
 }
 
 
